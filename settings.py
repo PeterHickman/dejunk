b83d73984c5821c12787efe39fed36777c79e3c3
Peter Hickman <peterhickman386@googlemail.com>
2017-10-12 22:14:54 +0100

    also part of the site

---
+++ settings.py
@@ -0,0 +1,10 @@
+# Database
+CONNECTION_STRING = 'postgresql://peterhickman@localhost:5432/flickr_development'
+
+# Images
+SOURCE_PATH = '/Volumes/Spare/Flickr/import/'
+DESTINATION_ROOT = '/Volumes/Spare/Flickr/'
+
+# Display
+IMAGES_A_PAGE = 72
+IMAGES_A_ROW = 12
