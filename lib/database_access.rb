6eb75e74fea13b42de0db958c2a63b62154310bf
Peter Hickman <peterhickman386@googlemail.com>
2021-01-03 18:50:55 +0000

    move stuff

--- lib/database_access.rb
+++ lib/database_access.rb
@@ -1,3 +1,5 @@
+require 'tags'
+
 class DatabaseAccess
   def initialize(dcs)
     @db = Sequel.connect(dcs)
@@ -16,11 +18,24 @@ class DatabaseAccess
   end
 
   def set_to_deleted(photo_id)
-    @db[:photos].where(photo_id: photo_id).update(status: 'deleted')
+    @db[:photos].where(id: photo_id).update(status: 'deleted')
   end
 
   def add_new_photo(filename, othername, file_size)
     @db[:photos].insert(filename: filename, othername: othername, status: 'unknown', file_size: file_size)
   end
-end
 
+  def set_size(photo_id, file_size)
+    @db[:photos].where(id: photo_id).update(file_size: file_size)
+  end
+
+  def add_tag_to_photo(photo_id, tag)
+    name, display = Tags.format(tag)
+
+    row = @db[:tags].where(photo_id: photo_id, name: name).first
+
+    return if row
+
+    @db[:tags].insert(photo_id: photo_id, name: name, display: display)
+  end
+end
