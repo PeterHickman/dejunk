d455f1701335fa0d0c803bcc22511cb60da37979
Peter Hickman <peterhickman386@googlemail.com>
2021-01-03 18:53:39 +0000

    resize is its own thing

--- import.rb
+++ import.rb
@@ -8,16 +8,7 @@ require 'fileutils'
 $LOAD_PATH << './lib'
 
 require 'database_access'
-
-def resize(source, destination, pixels)
-  s1 = "#{pixels}x#{pixels}"
-  s2 = "#{pixels * 2}x#{pixels * 2}"
-
-  puts("Resizing to create #{destination} at #{s1}")
-  cmd = "convert -define jpeg:size=#{s2} -auto-orient #{source}'[0]' -thumbnail '#{s1}>' -background transparent -gravity center -extent #{s1} #{destination}"
-
-  system(cmd)
-end
+require 'resize'
 
 config = YAML.load_file('config.yaml')
 
